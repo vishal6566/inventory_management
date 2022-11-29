f12a9e7028167a91133c82b46b6f7904cd8fdc69
Vishal <vishaldas6566@gmail.com>
2022-11-30 00:47:39 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
-console.log("hi")
+const express=require("express")
+const dotenv=require("dotenv").config()
+const mongoose=require("mongoose")
+const bodyParser=require("body-parser")
+const cors=require("cors")
+
+
+const app=express()
+
+const PORT=process.env.PORT || 5000;
+
+//connect to mongodb and start server
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server running on http://localhost:${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(err)
+})
+
