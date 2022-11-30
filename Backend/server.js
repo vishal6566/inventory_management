834beb9b0a33a8738af10c3eeeca9661178ec78b
Vishal <vishaldas6566@gmail.com>
2022-11-30 14:26:32 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,34 @@ const dotenv=require("dotenv").config()
 const mongoose=require("mongoose")
 const bodyParser=require("body-parser")
 const cors=require("cors")
+const userRoute=require("./routes/userRoute")
+const errorHandler=require("./middleWares/errorMiddleware")
 
 
 const app=express()
 
-const PORT=process.env.PORT || 5000;
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//routes middleware
+app.use("/api/users",userRoute)
+
+
+//routes
+app.get("/",(req,res)=>{
+    res.send("homepage")
+})
+
+//error middleware
+app.use(errorHandler)
+
+
 
 //connect to mongodb and start server
-
+const PORT=process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     app.listen(PORT,()=>{
         console.log(`server running on http://localhost:${PORT}`)
