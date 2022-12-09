ae60b9e72792f48770fb24e00268e39825fb03df
Vishal <vishaldas6566@gmail.com>
2022-12-09 10:48:18 +0530

    added

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const router=express.Router()
-const {registerUser, loginUser, logout, getUser, loginStatus}=require("../controllers/userController")
+const {registerUser, loginUser, logout, getUser, loginStatus, updateUser}=require("../controllers/userController")
 const protect = require("../middleWares/authMiddleware")
 
 
@@ -9,5 +9,6 @@ router.post("/login",loginUser)
 router.get("/logout",logout)
 router.get("/getuser",protect,getUser)
 router.get("/loggedin",loginStatus)
+router.patch("/updateuser",protect,updateUser)
 
 module.exports=router
